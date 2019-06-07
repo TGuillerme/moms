ab95afa8f79ec0faede09b7ace9528cabae9816c
TGuillerme <guillert@tcd.ie>
2019-06-07 16:19:00 +1000

    fixed moms paths

--- R/simulation.utilities.R
+++ R/simulation.utilities.R
@@ -166,12 +166,12 @@ simulation.rarefaction <- function(remove, replicates, metrics_list, what) {
 
 
 ## Save/load simulation results
-save.results <- function(results, path = "../data/processed") {
+save.results <- function(results, path = "../data/processed/") {
     match_call <- match.call()
     save(results, file = paste0(path, as.character(match_call$results), ".Rda"))
 }
 
-load.results <- function(results, path = "../data/processed") {
+load.results <- function(results, path = "../data/processed/") {
     load(paste0(path, results, ".Rda"))
     output <- results
     return(output)
