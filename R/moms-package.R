422a8e374420d7a65590c06edae7714e52fc81b4
TGuillerme <guillert@tcd.ie>
2019-07-12 16:41:30 +1000

    added demo data

---
+++ R/moms-package.R
@@ -0,0 +1,31 @@
+#' Measuring Occupancy of Multidimensional Spaces
+#' 
+#' A shiny app for measuring occupancy of multidimensional spaces
+#' 
+#' @name moms-package
+#'
+#' @docType package
+#'
+#' @author Thomas Guillerme <guillert@@tcd.ie>
+#'
+
+NULL
+
+#' Demo datasets
+#'
+#' A set six trait spaces with different groups and different dimensions
+#'
+#' \itemize{
+#'   \item \code{Beck2014} @@@
+#'   \item \code{Wright2017} @@@
+#'   \item \code{Marcy2016} @@@
+#'   \item \code{@@@} @@@
+#'   \item \code{Jones2015} @@@
+#'   \item \code{@@@} @@@
+#' }
+#'
+#' @format @@@
+#' @source @@@
+#' @references @@@
+#' @name Demo_data
+NULL
