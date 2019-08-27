40a58cae18213fdace43658fe437c8117933135b
TGuillerme <guillert@tcd.ie>
2019-08-27 16:56:02 +1000

    Updated list of metrics

--- inst/list.of.metrics.R
+++ inst/list.of.metrics.R
@@ -20,17 +20,19 @@ convhull <- function(matrix) {
 ## Small metrics list (for testing)
 ## Selected metrics
 metrics_list    <- list("func.disp" = func.disp,
-                       "sum.var"    = c(sum, variances),
-                       "ellips.vol" = ellipse.volume,
-                       "span.tree"  = function(matrix) sum(span.tree.length(matrix))/nrow(matrix),
-                       "ave.neigh"  = c(mean, neighbours),
-                       "av.displa"  = c(mean, displacements),
-                       "var.centre" = function(matrix) var(centroids(matrix, centroid = 0)))
+                        "sum.var"   = c(sum, variances),
+                        "sum.range" = c(sum, ranges),
+                        "ellips.vol"= ellipse.volume,
+                        "span.tree" = c(mean, span.tree.length),
+                        "func.eve"  = func.eve,
+                        "ave.neigh" = c(mean, neighbours),
+                        "av.displa" = c(mean, displacements))
 
 metric_names <- c("Average distance from centroid",
                   "Sum of variances",
+                  "Sum of ranges",
                   "Ellipsoid volume",
                   "Minimum spanning tree average distance",
+                  "Minimum spanning tree distances evenness",
                   "Average nearest neighbour distance",
-                  "Average displacements",
-                  "Variance of the distance from centre")
+                  "Average displacements")
