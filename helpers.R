ab2caf4a90f81630adafea4fc7dcf8982e604503
TGuillerme <guillert@tcd.ie>
2019-10-01 14:03:20 +1000

    corrected sourcedir

--- helpers.R
+++ helpers.R
@@ -35,6 +35,11 @@
 # input$scale_axis <- FALSE
 
 ## Get all functions
+sourceDir <- function(path, ...) {
+    for (name_file in list.files(path, pattern = "[.][RrSsQq]$")) {
+        source(file.path(path, name_file), ...)
+    }
+}
 sourceDir("R/")
 
 
